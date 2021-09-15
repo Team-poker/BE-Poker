b3999f7a0ea5ed5a552bb77b113c95a7a48f037f
Bekzhan Zhamantayev <bekzzhan31@gmail.com>
2021-09-15 18:17:30 +0600

    feat: add users.js

--- index.js
+++ index.js
@@ -1,17 +1,40 @@
 const app = require('express')();
 const http = require('http').Server(app);
-const io = require('socket.io')(http);
+const io = require('socket.io')(http, {
+  cors: {
+    origin: '*'
+  }
+});
 const port = process.env.PORT || 3000;
+const log = console.log;
 
-app.get('/', (req, res) => {
-  res.send('<h1>Planning poker chat</h1>');
-});
+const onConnection = (socket) => {
+  // выводим сообщение о подключении пользователя
+  log('User connected')
 
-io.on('connection', (socket) => {
-  socket.on('chat message', msg => {
-    io.emit('chat message', msg);
-  });
-});
+  // получаем название комнаты из строки запроса "рукопожатия"
+  const { lobbyId } = socket.handshake.query
+  // сохраняем название комнаты в соответствующем свойстве сокета
+  socket.lobbyId = lobbyId
+
+  // присоединяемся к комнате (входим в нее)
+  socket.join(lobbyId)
+
+  // регистрируем обработчики
+  // обратите внимание на передаваемые аргументы
+  registerMessageHandlers(io, socket)
+  registerUserHandlers(io, socket)
+
+  // обрабатываем отключение сокета-пользователя
+  socket.on('disconnect', () => {
+    // выводим сообщение
+    log('User disconnected')
+    // покидаем комнату
+    socket.leave(lobbyId)
+  })
+}
+
+io.on('connection', onConnection);
 
 http.listen(port, () => {
   console.log(`Socket.IO server running at http://localhost:${port}/`);
---
+++ users.js
@@ -0,0 +1,33 @@
+const usersList = [];
+
+// joins the user to the specific chatroom
+function joinUser(id, username, room) {
+  const user = { id, username, room };
+
+  usersList.push(user);
+  console.log(usersList, "usersList");
+
+  return user;
+}
+
+console.log("user out", usersList);
+
+// Gets a particular user id to return the current user
+function getCurrentUser(id) {
+  return usersList.find((user) => user.id === id);
+}
+
+// called when the user leaves the chat and its user object deleted from array
+function userDisconnect(id) {
+  const index = usersList.findIndex((user) => user.id === id);
+
+  if (index !== -1) {
+    return usersList.splice(index, 1)[0];
+  }
+}
+
+module.exports = {
+  joinUser,
+  getCurrentUser,
+  userDisconnect,
+};
